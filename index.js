296b1d08d74d4b91624ec359a79b856c4dba4d5c
alicelieutier <alice.lieutier@gmail.com>
2018-08-18 23:33:51 +0100

    first roll

--- index.js
+++ index.js
@@ -11,8 +11,38 @@ class Frame {
   constructor(id, previous_id = undefined) {
     this.id = id;
     this.past_rolls = [];
-    this.current_roll = undefined;
     this.score = 0;
     this.bonus = 0;
+  }
+
+  addRoll(roll) {
+    this.past_rolls.push(roll);
+    this.score += roll.nb_of_pins;
+  }
+}
+
+class Game {
+  constructor() {
+    this.past_frames = [];
+  }
+
+  addRoll(nb_of_pins) {
+    var roll = new Roll(nb_of_pins);
+    // is this the first frame
+    if (this.current_frame === undefined) {
+      this.current_frame = new Frame(0);
+      this.current_frame.addRoll(roll);
+    }
+  }
+
+  getScore() {
+    var score = 0;
+    for (var i = 0; i < this.past_frames.length; i++) {
+      score += this.past_frames[i].score;
+    }
+    if (this.current_frame != undefined) {
+      score += this.current_frame.score;
+    }
+    return score;
   }
 }
--- tests.js
+++ tests.js
@@ -3,13 +3,24 @@ var tests = [
     var roll = new Roll(4);
     assert(roll.nb_of_pins === 4);
   },
-  function testingNewFrame(){
+  function newFrame(){
     var frame = new Frame(23);
     assert(frame.id === 23);
     assert(frame.past_rolls.length === 0);
-    assert(frame.current_roll === undefined)
     assert(frame.score === 0);
     assert(frame.bonus === 0);
+  },
+  function newGame(){
+    var game = new Game();
+    assert(game.past_frames.length === 0);
+    assert(game.getScore() === 0);
+  },
+  function addFirstRoll(){
+    var game = new Game();
+    game.addRoll(6);
+    assert(game.past_frames.length === 0);
+    assert(game.current_frame.score === 6);
+    assert(game.getScore() === 6);
   },
 ];
 
